d0d942be03dfeac9e9d4add6360ff9851dd53323
dellaoctaviarisman <dellaoctaviarisman@gmail.com>
2023-03-08 13:04:16 +0700

    Initial commit

---
+++ src/della070323/KondisiOperator.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package della070323;
+
+/**
+ *
+ * @author LABP1KOMP
+ */
+public class KondisiOperator {
+    public static void main (String [] args){
+       
+       String status = "";
+       int grade = 80;
+       
+       status = (grade >= 60)?"Passed":"Fail";
+       
+       System.out.println( status );
+   } 
+    
+}
