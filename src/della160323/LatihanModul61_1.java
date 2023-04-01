f0c0bf19f3f92d30b22f52ef87c1581a36afea23
dellaoctaviarisman <127170979+dellaoctaviarisman@users.noreply.github.com>
2023-04-01 12:36:55 +0700

    update

--- src/della160323/LatihanModul61_1.java
+++ src/della160323/LatihanModul61_1.java
@@ -3,11 +3,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package della160323;
-
+import java.io.IOException;
+import java.io.Reader;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 /**
  *
  * @author Windows 10 Pro
  */
 public class LatihanModul61_1 {
     
+    public static void main(final String[] args) {
+        final BufferedReader dataIn = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            System.out.print("Entri nilai A : ");
+            final int nilai1 = Integer.parseInt(dataIn.readLine());
+            System.out.print("Entri nilai B : ");
+            final int nilai2 = Integer.parseInt(dataIn.readLine());
+            System.out.print("Entri nilai C : ");
+            final int nilai3 = Integer.parseInt(dataIn.readLine());
+            final int rata = (nilai1 + nilai2 + nilai3) / 3;
+            System.out.println("Rata rata : " + rata);
+            if (rata >= 60) {
+                System.out.println(":-)");
+            }
+            else {
+                System.out.println(":-(");
+            }
+        }
+        catch (IOException ex) {}
+    }
 }
